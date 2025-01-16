0f84d1f88d74ef955c63eb9c47ac7d07e54d4ace
mohbisher <mohammed@mohbisher.com>
2025-01-16 19:20:18 +0300

    feat: added new social links

--- src/components/Footer-arch/footer-arch.jsx
+++ src/components/Footer-arch/footer-arch.jsx
@@ -51,7 +51,7 @@ const FooterArch = () => {
                 }}
               >
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3623.9683489151614!2d46.67529531500036!3d24.71355198411248!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2f038476f60b5d%3A0x8804d0d89fd9e2b3!2sEasy%20Accounting%20Location!5e0!3m2!1sen!2ssa!4v1673306313045!5m2!1sen!2ssa"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3631.368449637666!2d39.6481875!3d24.4726875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15bdbfaba0aaff93%3A0x2bfcf4f2bcf7a32d!2z2LTYsdmD2Kkg2KfZitiy2Yog2KPZg9mI2YbYqtmG2KwgLSDYp9mE2YXYr9mK2YbYqSBFYXN5IEFjY291bnRpbmc!5e0!3m2!1sen!2ssa!4v1737044359094!5m2!1sen!2ssa"
                   width="100%"
                   height="250"
                   style={{ border: 0 }}
@@ -75,13 +75,13 @@ const FooterArch = () => {
                   <i className="fab fa-twitter"></i>
                 </a>
                 <a
-                  href="https://www.instagram.com/easyaccoksa/"
+                  href="https://www.instagram.com/easyaccounting_ksa/"
                   target="_blank"
                 >
                   <i className="fab fa-instagram"></i>
                 </a>
                 <a
-                  href="https://www.linkedin.com/company/easyaccksa/"
+                  href="https://www.linkedin.com/company/easy-accounting777/"
                   target="_blank"
                 >
                   <i className="fab fa-linkedin"></i>
